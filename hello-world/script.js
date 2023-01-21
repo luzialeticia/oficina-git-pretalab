a27e84dcf67c862b648c45dc9bf833fd5f958ddb
Leticia Luzia <leticialuzia89@gmail.com>
2023-01-21 17:04:57 -0300

    Hello world em JS para Pretalab

---
+++ hello-world/script.js
@@ -0,0 +1,6 @@
+alert("Olá Mundo!")
+
+const nomeUsuaria = prompt("Digite aqui o seu nome:")
+
+alert("Bem vinda ao meu primeiro site, " + nomeUsuaria)
+alert(`Bem vinda ao meu primeiro site, ${nomeUsuaria}`)
